15bbd4e46d0b654c2b9c691e1b553f05107dbb44
Martino Piccinato <martino.piccinato@gmail.com>
2018-03-07 12:07:28 +0100

    Forgot to export PostDeserializeListener

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export {SerializerModule} from './serializer.module';
 export {SerializerService} from './serializer.service';
 export {SerializerTransformer, SerializerExclude, SerializerReplace} from './serializer.metadata';
 export {SerializerDirection} from './serializer-direction';
+export {PostDeSerializeListener} from './post.deserialize.listener';
